80daca8b5fa4b823b87b576f595a8232015a4e2d
s0lver <rperez.github@gmail.com>
2016-02-05 15:17:27 -0600

    HotFix start datetime of trajectory

--- uploadFixMontoliou.php
+++ uploadFixMontoliou.php
@@ -11,12 +11,17 @@ if (isset($_POST['createTrajectory'])){
     $max_time = $_POST["maxTime"];
     $min_distance = $_POST["minDistance"];
 
-    $date = new DateTime("now");
-    $converted_date = $date->format('Y-m-d H:i:s');
+    $time_zone = new DateTimeZone("America/Mexico_City");
+    $date_now = new DateTime("now",$time_zone);
+    $converted_date = $date_now->format('Y-m-d H:i:s');
+
+    $log = new Logging();
+    $log->lfile('c:\\php-log\\mylogTrajectory.txt');
+    $log->lwrite("fecha es: " . $converted_date);
 
     insert_new_trajectory($converted_date, $converted_date, $min_distance, $min_time, $max_time);
 
-    echo 1;
+    echo "trajOk";
 }
 else {
     $last_trajectory = get_last_trajectory();
